6ae8d67acab2b45e62176f992929c8d0a21a83b5
Hals Henri <104424322+henrihals@users.noreply.github.com>
2022-06-20 20:26:48 +0200

    fin projet

---
+++ README.md
@@ -0,0 +1,25 @@
+#Mission objectives :
+
+In this challenge you will use and consolidate your knowledge on:
+
+A typical HTTP flow: send asynchronous requests to a remote server and process the results
+DOM manipulation: changing the DOM based on the results of the HTTP requests
+Learn to aggregate and parse data fetched from an API
+
+##The Mission :
+
+You have been sent abroad for a 10-month work mission. Your family and friends back home ask you about the weather where you live ALL. THE. *** TIME.
+Enough is enough, you decide to build a small web application for them so that you can free your time to talk about more interesting topics.
+
+##Specifications :
+
+In the home page the user can enter the city of their choice (think of the right HTML elements here)
+On clicking the SUBMIT button or pressing ENTER the application will display the weather for the next 5 days
+
+##Technology :
+
+HTML - SASS - JavaScript
+
+**Github : 
+
+**Internet : 
--- script.js
+++ script.js
@@ -1,133 +1,119 @@
-function capitalize(str) {
-    return str[0].toUpperCase() + str.slice(1);
-}
-/*
-function main() {
-    
-    fetch(`https://api.ipify.org?format=json`) // 
-        .then(resultat => resultat.json()) // 
-        .then(json => {
-            const ip = json.ip; //
-            
-            fetch(`https://ip-api.com/#` + ip)
-                
-        })
-}
-*/
-const submit = document.querySelector(".submit");
+const submit = document.querySelector(".submit"); 
 
-submit.addEventListener("click", async () => {
-    console.log("coucou")
+// Quand submit est cliqué
+submit.addEventListener("click", async () => { 
     let firstCity = document.querySelector(".ville1").value;
-    console.log(firstCity);
+
+    // On récupère la ville de départ et lance la recherche des informations
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${firstCity}&units=metric&appid=8384ea512daba904b5dfb5f71d2dc3c8`)
         .then ((response) => 
             response.json().then((data) => {
                 console.log(data);
                 const clouds = data.weather[0].description;
                 const temp= data.main.temp;
-                document.querySelector("#humidity").innerHTML = "<i class='fa-solid fa-droplet'></i>" + data.main.humidity + " %";
-                const wind = data.wind.speed;                
-           
+                const wind = data.wind.speed;
                 
+                // lancement de la fonction pour afficher les icons
+                const icon = data.weather[0].icon;
+                const url = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+                const image = document.createElement("img");
+                image.src = url;
+                document.querySelector("#icons").appendChild(image);
+                
+                // Choix des background en fonction de la description du temps
                 switch (clouds) {
                     case "clear sky":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-sun'></i>" + clouds;
                         document.body.style.backgroundImage = "url(./src/image/sun.jpg)";
                         break;
                     case "few clouds":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-cloud'></i>" + clouds;
                         document.body.style.backgroundImage = "url(./src/image/nuage.jpg)";
                         break;
                     case "scattered clouds":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-cloud'></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/nuages_eparpilles.jpg)";
                         break;
                     case "broken clouds":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-cloud-showers-heavy'></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/broken_clouds.jpg)";
                         break;
                     case "shower rain":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-cloud-showers-heavy'></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/shower_rain.jpg)";
                         break;
                     case "rain":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-cloud-rain'></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/rain.jpg)";
                         break;
                     case "thunderstorm":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-cloud-bolt'></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/rain.jpg)";
                         break;
                     case "snow":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-snowflake></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/snow.jpg)";
                         break;
                     case "mist":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-smog'></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/mist.jpg)";
                         break;
                     case "drizzle":
-                        document.querySelector("#clouds").innerHTML = "<i class='fa-solid fa-cloud-showers-heavy'></i>" + clouds;
+                        document.body.style.backgroundImage = "url(./src/image/drizzle.jpg)";
                         break;
                     default:
-                        document.querySelector("#clouds").innerHTML = clouds;
+                        document.body.style.backgroundImage = "url(./src/image/nuage.jpg)";
                         break;
                 }
-                document.querySelector("#temps").innerHTML = "<i class='fa-solid fa-temperature-full'></i>" + Math.round(temp) + " °C";
-                document.querySelector("#wind").innerHTML = "<i class='fa-solid fa-wind'></i>" + Math.round(wind*3.6) + " km/h";
-            })
-            );
-    
-    
 
-    /*
-    
-    
-    let response1 = await askCity1.json();
-    console.log(response1);
-    let temperate = response1.main.temp;
-    console.log(temperate);
-    let weather = response1.weather[0].main;
-    console.log(weather);
+                // Affichage des informations demandées (température, vent et humidité)
+                document.querySelector("#temps").innerHTML = "<i class='fa-solid fa-temperature-full'></i>" + Math.round(temp) + "° C ";
+                document.querySelector("#humidity").innerHTML = "<i class='fa-solid fa-droplet'></i>" + data.main.humidity + " % ";
+                document.querySelector("#wind").innerHTML = "<i class='fa-solid fa-wind'></i>" + Math.round(wind*3.6) + " km/h ";
 
-    // création de la zone température
-    let sectionTemp = document.createElement("section");
-    console.log("1");
-    let main = document.querySelector("main");
-    console.log("2");
-    main.appendChild(sectionTemp);
-    console.log("3");
-    let pTemp = document.createElement("p");
-    console.log("4");
-    sectionTemp.appendChild(ptemp);
-    console.log("6");
-    pTemp.innerText = temperate; //souci à ce niveau
-    console.log("5");
-     
-
-    /* récupération de données
-    
-        const cityName = data.name;
-        console.log(cityName);
-        const temperate = data.main.temp;
-        console.log(temperate);
-        const condition = data.wheather[o].main
-        console.log(condition);
-    }
-
-    console.log(recup);
-    console.log(cityName);
-    console.log(temperate);
-    console.log(condition);
-*/
-/*
-    // création de la zone température
-    let sectionTemp = document.createElement("section");
-    console.log("1");
-    let main = document.querySelector("main");
-    console.log("2");
-    main.appendChild(sectionTemp);
-    console.log("3");
-    let pTemp = document.createElement("p");
-    console.log("4");
-    pTemp.innerText = response1.name; //souci à ce niveau
-    console.log("5");
-    sectionTemp.appendChild(ptemp);
-    console.log("6");   
-    */
-}
-)
+            // lancement de la recherche des informations pour les 5 jours à venir
+            fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${firstCity}&units=metric&appid=8384ea512daba904b5dfb5f71d2dc3c8`)
+                .then ((response) => 
+                    response.json().then((data) => {
+                console.log(data);
+                const clouds1 = data.list[7].main.temp;
+                const icon1 = data.list[7].weather[0].icon;
+                const url1 = `http://openweathermap.org/img/wn/${icon1}@2x.png`;
+                img1.src = url1;
+                const day1 = new Date
+                day1.setTime(day1.getTime() + 24 * 3600 * 1000);
+                document.querySelector("#days1").innerHTML = day1.getDate() + " / " + day1.getMonth();
+                const clouds2 = data.list[15].main.temp;
+                const icon2 = data.list[15].weather[0].icon;
+                const url2 = `http://openweathermap.org/img/wn/${icon2}@2x.png`;
+                img2.src = url2;
+                const day2 = new Date
+                day2.setTime(day2.getTime() + 48 * 3600 * 1000);
+                document.querySelector("#days2").innerHTML = day2.getDate() + " / " + day2.getMonth();
+                const clouds3 = data.list[23].main.temp;
+                const icon3 = data.list[23].weather[0].icon;
+                const url3 = `http://openweathermap.org/img/wn/${icon3}@2x.png`;
+                img3.src = url3;
+                const day3 = new Date
+                day3.setTime(day3.getTime() + 72 * 3600 * 1000);
+                document.querySelector("#days3").innerHTML = day3.getDate() + " / " + day3.getMonth();
+                const clouds4 = data.list[31].main.temp;
+                const icon4 = data.list[31].weather[0].icon;
+                const url4 = `http://openweathermap.org/img/wn/${icon4}@2x.png`;
+                img4.src = url4;
+                const day4 = new Date
+                day4.setTime(day4.getTime() + 96 * 3600 * 1000);
+                document.querySelector("#days4").innerHTML = day4.getDate() + " / " + day4.getMonth();
+                const clouds5 = data.list[39].main.temp;
+                const icon5 = data.list[39].weather[0].icon;
+                const url5 = `http://openweathermap.org/img/wn/${icon5}@2x.png`;
+                img5.src = url5;
+                const day5 = new Date
+                day5.setTime(day5.getTime() + 120 * 3600 * 1000);
+                document.querySelector("#days5").innerHTML = day5.getDate() + " / " + day5.getMonth();
+                
+                // Affichage de la température pour les 5 jours à venir
+                document.querySelector("#cloud1").innerHTML = Math.round(clouds1) + "° C ";
+                document.querySelector("#cloud2").innerHTML = Math.round(clouds2) + "° C ";
+                document.querySelector("#cloud3").innerHTML = Math.round(clouds3) + "° C ";
+                document.querySelector("#cloud4").innerHTML = Math.round(clouds4) + "° C ";
+                document.querySelector("#cloud5").innerHTML = Math.round(clouds5) + "° C ";
+                
+                // changement de style pour le titre et input
+                document.querySelector("h1").style.color = "#000000";
+                document.querySelector("input").style.backgroundColor = "#ffffff";
+            }))
+        })
+    )
+});
